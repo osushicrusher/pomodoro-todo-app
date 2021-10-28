40c7b473607b2c91e3cdec9f1011068403c58445
h.yoshino <osushicrusher@gmail.com>
2021-10-29 01:50:16 +0900

    ✨ コンポーネント作成

---
+++ laravel/resources/js/components/parts/Buttons/ButtonPrimary.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    text: String
+  },
+  data() {
+    return {
+
+    };
+  },
+  created: function() {
+    console.log(this.text)
+  },
+  methods: {
+
+  }
+};
+</script>
---
+++ laravel/resources/js/components/parts/Calendar.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-calendar />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      attrs: [
+        {
+          key: 'today',
+          highlight: {
+            backgroundColor: '#ff8080',
+          },
+          dates: new Date(),
+          popover: {
+            label: 'メッセージを表示できます',
+          },
+        }
+      ],
+    }
+  }
+};
+</script>
