43665e8c425b2ed9c0151f2f261f342a536d50b0
h.yoshino <osushicrusher@gmail.com>
2021-11-20 22:26:48 +0900

    buttons

--- laravel/resources/js/components/parts/Buttons/SendEmailButton.vue
+++ laravel/resources/js/components/parts/Buttons/SendEmailButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full h-12 w-12 flex items-center justify-center...">
-    <v-icon name="user-plus"></v-icon>
+    <v-icon name="envelope"></v-icon>
   </button>
 </template>
 
